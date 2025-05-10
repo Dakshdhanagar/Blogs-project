1d68bae818904c395661a9cbeec029c0843f007c
Daksh Dhanagar <dakshoffical770@gmail.com>
2025-05-10 10:42:52 +0530

    2commit

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ const path = require("path");
 app.use(express.urlencoded({extended : true}));
 
 app.set("view engine","ejs");
-app.set("view",path.join(__dirname,"views"));
+app.set("views",path.join(__dirname,"views"));
  
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/",(req,res) =>{
-    res.send("server working properly");
+//making array for working as a database;
+let posts =[
+    {
+        username : "Daksh",
+        content  : "I love coding"
+    },
+    {
+        username : "Sachin",
+        content  : "I love web development"
+    },
+    {
+        username : "Kunal",
+        content  : "I love playing game"
+    }
+]
+
+
+app.get("/posts",(req,res) =>{
+    res.render("index.ejs",{ posts });
 })
 
 
